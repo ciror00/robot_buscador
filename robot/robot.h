eecc5ab1a0ed95b71116d43d5ab101690d0091b3
Ciro Edgardo Romero <ciror00@gmail.com>
2019-06-28 12:46:28 -0300

    Se sube codigo principal del proyecto

---
+++ robot/robot.h
@@ -0,0 +1,39 @@
+/*
+  D0 = GPIO16;
+  D1 = GPIO5;
+  D2 = GPIO4;
+  D3 = GPIO0;
+  D4 = GPIO2;
+  D5 = GPIO14;
+  D6 = GPIO12;
+  D7 = GPIO13;
+  D8 = GPIO15;
+  D9 = GPIO3;
+  D10 = GPIO1;
+  LED_BUILTIN = GPIO16
+ */
+#include "Auto.h"
+#include "Distancia.h"
+#include "Luz.h"
+#include "MQTT.h"
+
+// Motor A
+const int ENA = D8;
+const int IN1 = D7;
+const int IN2 = D6;
+
+// Motor B
+const int ENB = D3;
+const int IN3 = D4;
+const int IN4 = D5;
+
+const int trigger = D1;   //Pin digital 2 para el Trigger del sensor
+const int echo = D0;   //Pin digital 3 para el Echo del sensor
+
+const int LUZ = D2;
+
+char* red = "";
+char* pass = "";
+char* server = "";
+
+long detectar;
---
+++ robot/robot.ino
@@ -0,0 +1,35 @@
+#include "robot.h"
+
+#define PROD false
+#define SERIAL_BEGIN if(!PROD) Serial.begin(115200)
+#define SERIAL_PRINT(MESSAGE) if(!PROD) Serial.println(MESSAGE)
+
+Auto autito;
+Distancia distancias;
+MQTT mqtt;
+
+
+void setup(){
+  SERIAL_PRINT("Inicio");
+  autito.begin();
+  distancias.begin(trigger, echo);
+  mqtt.begin(red, pass, server);
+  mqtt.IP();
+  mqtt.Reconectar();
+  mqtt.Publicar("Hola mundo");
+}
+
+void loop(){
+  detectar = distancias.Medicion();
+  if(detectar < 10){
+    autito.Detener();
+    autito.Adelante();
+  }else{
+    autito.Girar();
+  }
+  attachInterrupt(digitalPinToInterrupt(LUZ), encontrar, FALLING);
+}
+
+void encontrar(){
+  autito.Volver();
+}
